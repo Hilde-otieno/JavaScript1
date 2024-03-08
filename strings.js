ea4def09e6effd4675fb8daca732575c40d5ad16
Hilde-Otieno <meghanadhiambootieno@gmail.com>
2024-03-08 17:14:53 +0300

    add file

---
+++ strings.js
@@ -0,0 +1,30 @@
+let a='extravaganza'
+let b=a.slice(8,12)
+console.log(b);
+
+let string='The quick fox jumped over the lazy dog'
+let stringToAdd="eat"
+let indexPosition=(4)
+newString=string.substring(4/indexPosition)+stringToAdd+string.substring(indexPosition)
+console.log(newString)
+
+let story='The quick brown fox jumps over the lazy dog'
+let c='the'
+let d='brown'
+console.log((story.match(/the/)).length);
+console.log((story.match(/brown/)).length);
+
+const string1='The pupils are reading in the library'
+let e=string1.includes('are')
+console.log(e);
+
+const string2='The child was sitting on the table before'
+let f=string2.includes('sitting')
+console.log(f);
+
+const g='wonderful';
+console.log(g.toUpperCase())
+
+const h='amazing';
+console.log(h.toLowerCase())
+
